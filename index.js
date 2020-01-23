09405151bd0b4cc58664797c2669e6399efab510
Miguelsiqueiros <miguel.siqueiros17@tectijuana.edu.mx>
2020-01-23 15:42:00 -0800

    Change env for port to PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const Port = process.env.Port || 5000;
+const Port = process.env.PORT || 5000;
 
 app.get('/*', (req, res) => res.send('OK'));
 
